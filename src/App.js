8c12b13203f1ef8946bbd04c6cbe9b828e70c231
EliptickBlock5 <nbjohnson@gmail.com>
2021-06-20 03:57:58 -0700

    rough draft app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
+import Nav from "./components/Nav";
 import About from "./components/About";
+import Portfolio from "./components/Projects";
+import ContactForm from "./components/Contact";
+import Resume from "./components/Resume";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
 
 function App() {
+  const [aboutSelected, setAboutSelected] = useState(true);
+  const [portfolioSelected, setPortfolioSelected] = useState(false);
+  const [contactSelected, setContactSelected] = useState(false);
+  const [resumeSelected, setResumeSelected] = useState(false);
+
   return (
-    <div>
+    <div className="page-container">
+    <div className="content-wrap">
+      <Header
+        aboutSelected={aboutSelected}
+        setAboutSelected={setAboutSelected}
+        portfolioSelected={portfolioSelected}
+        setPortfolioSelected={setPortfolioSelected}
+        contactSelected={contactSelected}
+        setContactSelected={setContactSelected}
+        resumeSelected={resumeSelected}
+        setResumeSelected={setResumeSelected}
+      ></Header>
       <main>
-        <About></About>
+        {aboutSelected ? (
+          <>
+            <About></About>
+          </>
+        ) : (
+          <></>
+        )}
+        {portfolioSelected ? (
+          <>
+            <Portfolio></Portfolio>
+          </>
+        ) : (
+          <></>
+        )}
+        {contactSelected ? (
+          <>
+            <ContactForm></ContactForm>
+          </>
+        ) : (
+          <></>
+        )}
+        {resumeSelected ? (
+          <>
+            <Resume></Resume>
+          </>
+        ) : (
+          <></>
+        )}
       </main>
     </div>
-  );
+    <Footer></Footer>
+  </div>
+);
 }
+
+
+export default App;
--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,19 +1,37 @@
-import React from 'react'
-import coverImage from "../../assets/images/selfie.jpg";
+import React from "react";
 
 function About() {
-
   return (
-    <section className="my-5" >
-      <h1 id="about">About Nate Johnson</h1>
-      <img src={coverImage} className="my-2" style={{ width: "100%" }} alt="cover" />
-      <div className="my-2">
-        <p>
-          Placeholder text, placeholder text
-      </p>
+    <section className="about">
+      <h1 id="about" className="px-1">About Nate Johnson</h1>
+      <div className="px-1">
+        <img
+          src={require("../../assests/images/selfie.jpg").default}
+          alt="self-portrait my-2"
+          className="self-portrait px-1"
+        ></img>
+        <p className="px-1">
+          Hello, my name is Nate Johnson and I decided to make the move of 
+          becoming a web developer after 10 years in the insurance industy.
+          I have attended the University of Arizona Coding Boot Camp with
+          the goal of working as a software developer. 
+          
+          My prior experience has prepared me well for the attention to detail 
+          required in software development and the team work and problem solving skills 
+          required to be a great developer. I also enjoy continuous learning and 
+          staying up-to-date on current technologies.
+
+          In my spare time I enjoy traveling, and studying history and past cultures. 
+
+
+
+
+
+
+        </p>
       </div>
     </section>
-  )
+  );
 }
 
-export default About
+export default About;
