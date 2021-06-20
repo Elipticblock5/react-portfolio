c988ebb5c5927b51bd4a7649cfce272e1ab9856f
EliptickBlock5 <nbjohnson@gmail.com>
2021-06-20 03:19:35 -0700

    added selfie, changed styling

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React from 'react'
+import coverImage from "../../assets/images/selfie.jpg";
 
 function About() {
+
   return (
-    <section>
+    <section className="my-5" >
       <h1 id="about">About Nate Johnson</h1>
+      <img src={coverImage} className="my-2" style={{ width: "100%" }} alt="cover" />
+      <div className="my-2">
+        <p>
+          Placeholder text, placeholder text
+      </p>
+      </div>
     </section>
-  );
+  )
 }
 
-export default About;
+export default About
