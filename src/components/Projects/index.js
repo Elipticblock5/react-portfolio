ede5f851c393efb1f202bf510edc983cd889da21
EliptickBlock5 <nbjohnson@gmail.com>
2021-06-20 22:13:20 -0700

    added tech description to portfolio

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -8,6 +8,7 @@ function ProjectsCompleted() {
     {
       name: "Javascript Quiz APP",
       desc: "A fun and silly online quiz game with local storage for high scores",
+      tech: "Tech Used: HTML, CSS, Javascript",
       projectLink: "https://elipticblock5.github.io/hw04WebAPI/",
       githubLink: "https://github.com/Elipticblock5/hw04WebAPI",
     },
@@ -18,20 +19,23 @@ function ProjectsCompleted() {
     {
       name: "Weather Dashboard",
       desc: "A weather app using openweather API, and showing search history",
+      tech: "Tech Used: HTML, CSS, Bootsrap, JQuery, API",
       projectLink: "https://elipticblock5.github.io/hw06weather/",
       githubLink: "https://github.com/Elipticblock5/hw06weather",
     },
 
     {
       name: "AZ 4 Locals",
-      desc: "An interactive blog made with Node.js, Express.js, and Handlebars",
+      desc: "An interactive blog made with custom login",
+      tech: "Tech Used: HTML, CSS, Node, Express, Handlebars.js",
       projectLink: "https://agile-cliffs-08028.herokuapp.com/",
       githubLink: "https://github.com/marcysko/az4locals",
     },
 
     {
       name: "MongoDB Social Media Backend",
-      desc: "A Node and Mongo with Mongoose social media backend",
+      desc: "A backend environment for a social media webiste",
+      tech: "Tech Used: Node, MongoDB, Mongoose, Express",
       projectLink: "https://www.youtube.com/watch?v=X5d1MYNaAUM",
       githubLink: "https://github.com/Elipticblock5/hw18mongo",
     },
@@ -39,6 +43,7 @@ function ProjectsCompleted() {
     {
       name: "Nate-Budget-Tracker",
       desc: "A progressive web app with online and offline capability",
+      tech: "Tech Used: HTML, CSS, Express, Mongoose, MongoDB, Node, Heroku",
       projectLink: "https://nate-budget-tracker.herokuapp.com/",
       githubLink: "https://github.com/Elipticblock5/hw19pwa",
     },
@@ -59,6 +64,7 @@ function ProjectsCompleted() {
             />
             <h3 className="block">{project.name}</h3>
             <h4 className="block">{project.desc}</h4>
+            <h4 className="block">{project.tech}</h4>
             <a className="block" href={project.projectLink}>Project Link</a>
             <a className="block" href={project.githubLink}>GitHub Link</a>
           </div>
